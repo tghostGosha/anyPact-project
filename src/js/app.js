a78aef88a1a23618206f2e0becd94a0297f3a6f0
tghostGosha <tghost@mail.ru>
2022-08-19 17:26:20 +0300

    done

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 import * as flsFunctions from "./modules/functions.js";
-// import $ from "jquery"
+import $ from "jquery"
 // import { Inputmask } from "inputmask";
 // import Swiper, { Navigation, Pagination } from 'swiper';
 // import JustValidate from 'just-validate';
@@ -7,7 +7,45 @@ import * as flsFunctions from "./modules/functions.js";
 // Swiper.use([Navigation, Pagination])
 
 flsFunctions.isWebp()
+function OpenModalWindow(el){
+	CloseModalWindow();
+	let modal = $('.modal-block');
+	modal.addClass('open');
+	el.show();
+}
+function CloseModalWindow(){
+	let modal = $('.modal-block');
+	let forms = $('form', modal);
+	let formsBlocks = $('.modal-window-content > div', modal)
+	modal.removeClass('open');
+	forms.each(function(){this.reset()});
+	formsBlocks.each(function(){$(this).hide()});
+}
+$( document ).ready(function (){
+	$(document).on('click', '.open-test1', function (){
+		OpenModalWindow($('.test1'));
+	});
+	$(document).on('click', '.open-test2', function (){
+		OpenModalWindow($('.test2'));
+	});
+      $(document).on('click', '.open-test3', function (){
+		OpenModalWindow($('.test3'));
+	});
+      $(document).on('click', '.open-test4', function (){
+		OpenModalWindow($('.test4'));
+	});
+      $(document).on('click', '.open-test5', function (){
+		OpenModalWindow($('.test5'));
+	});
 
+
+	$(document).on('click', '.modal-close, .modal-bg', function (){
+		CloseModalWindow();
+	});
+	$(document).on('click', '.modal-window', function (e){
+		e.stopPropagation();
+	});
+});
 
 //=========swiper hero=======
 // const heroSwiper = new Swiper('.hero-swiper', {
